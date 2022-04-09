8953d2bba9b78fa69c9d988f2a8ae49a30480390
petervekony <peter.vekony@protonmail.com>
2022-04-09 18:26:58 +0200

    First commit

---
+++ printcombn.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintCombN() {
+	//var numbers []rune = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	z01.PrintRune('u')
+}
+
+func main() {
+	PrintCombN()
+}
